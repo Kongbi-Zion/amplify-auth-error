d8cfc38554d2fa72ba07bdd7ff47f19352fe3444
Kongbi zion nfor <kongbizion3@gmail.com>
2023-09-22 13:49:04 +0100

    imported auth in root app

--- app.vue
+++ app.vue
@@ -3,3 +3,12 @@
     <NuxtPage />
   </div>
 </template>
+<script>
+import { Auth } from "aws-amplify";
+export default {
+  mounted() {
+    const user = Auth.currentAuthenticatedUser();
+    console.log("user : ", user);
+  },
+};
+</script>
